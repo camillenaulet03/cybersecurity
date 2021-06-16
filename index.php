e0bc0432d9a67421c43cba64b6918da2d0d00be2
Johan <johdel390@gmail.com>
2021-06-16 15:00:38 +0200

    Mieux ?

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+include_once("./.inc/EnvBuilder.php");
+(new Env('.env'))->load();
 include('database/classes/db_manager.class.php');
 include('.inc/manager.php');
+var_dump(getenv("DB_USERNAME"));
 if(isset($_GET["connexion2"])){
 $db = new DB_Manager();
 $db->insertToken('test');
